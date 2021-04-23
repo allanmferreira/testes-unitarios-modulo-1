179de11b01ce2a04ae9ea86075bf64e31469c4a9
Allan Megiani Ferreira <allan_ssk@hotmail.com>
2021-04-22 23:50:30 -0300

    Aula 5
    Adicionando settings do prettier e atualizando arquivos

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -2,9 +2,9 @@ module.exports.sum = (num1, num2) => {
     const int1 = parseInt(num1);
     const int2 = parseInt(num2);
 
-    if(Number.isNaN(int1) || Number.isNaN(int2)){
-        throw new Error('Please check your inputs')
+    if (Number.isNaN(int1) || Number.isNaN(int2)) {
+        throw new Error('Please check your inputs');
     }
 
     return int1 + int2;
-}
+};
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,18 +1,13 @@
-const {queryString} = require('./queryString');
+const { queryString } = require('./queryString');
 
 describe('Object do query string', () => {
     it('should create a query string when a object is received', () => {
         const obj = {
             name: 'Allan',
-            profession: 'Developer'
-        }
-        
-        expect(queryString(obj)).toBe(
-            'name=Allan&profession=Developer'
-        );
+            profession: 'Developer',
+        };
+        expect(queryString(obj)).toBe('name=Allan&profession=Developer');
     });
 });
 
-describe('Query string to object', () => {
-    
-});
+describe('Query string to object', () => {});
