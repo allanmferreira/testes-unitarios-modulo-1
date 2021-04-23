1159d5313ad375dc781bf5122464db1e1dbdfbad
Allan Megiani Ferreira <allan_ssk@hotmail.com>
2021-04-23 00:10:10 -0300

    Aula 7
    Teste de throw error

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -8,6 +8,28 @@ describe('Object do query string', () => {
         };
         expect(queryString(obj)).toBe('name=Allan&profession=Developer');
     });
+
+    it('should create a valid query string even when a array is passed as a value', () => {
+        const obj = {
+            name: 'Allan',
+            abilities: ['Vue', 'CSS', 'JS'],
+        };
+        expect(queryString(obj)).toBe('name=Allan&abilities=Vue,CSS,JS');
+    });
+
+    it('should throw a error when an object is passed as a value', () => {
+        const obj = {
+            name: 'Allan',
+            abilities: {
+                first: 'Vue',
+                second: 'CSS',
+                third: 'JS',
+            },
+        };
+        expect(() => {
+            queryString(obj);
+        }).toThrowError();
+    });
 });
 
 describe('Query string to object', () => {});
